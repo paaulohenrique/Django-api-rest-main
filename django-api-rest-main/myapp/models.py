02269a410571f5ccdbce5e63597cea12309beb10
Paaulohenrique <paulosiilva180@gmail.com>
2025-09-03 20:01:35 -0300

    Second commit

--- django-api-rest-main/myapp/models.py
+++ django-api-rest-main/myapp/models.py
@@ -5,6 +5,9 @@ class Client(models.Model):
     email = models.EmailField(unique=True)
     birth_date = models.DateField(null=False, blank=False)
 
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
