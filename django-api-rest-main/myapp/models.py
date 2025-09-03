2ee743acbe9106d27410b7c9931df4aa6b785087
Paaulohenrique <paulosiilva180@gmail.com>
2025-09-03 19:57:48 -0300

    first commit

--- django-api-rest-main/myapp/models.py
+++ django-api-rest-main/myapp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Client(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
+    birth_date = models.DateField(null=False, blank=False)
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
